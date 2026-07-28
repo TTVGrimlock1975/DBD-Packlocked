13fa3d6cf3e5011d7a0c1efef449d3e21db6c630
TTVGrimlock1975 <slenderp1975@gmail.com>
2026-07-28 17:59:03 -0400

    Add files via upload

---
+++ data/perks_02.js
@@ -0,0 +1,105 @@
+gameData.addons.push(
+    {
+        name: "Common Med-Kit Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Uncommon Med-Kit Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Rare Med-Kit Add-on",
+        rarity: "Rare",
+        type: "Addon"
+    },
+    {
+        name: "Very Rare Med-Kit Add-on",
+        rarity: "Epic",
+        type: "Addon"
+    },
+    {
+        name: "Ultra Rare Med-Kit Add-on",
+        rarity: "Legendary",
+        type: "Addon"
+    },
+
+    {
+        name: "Common Flashlight Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Uncommon Flashlight Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Rare Flashlight Add-on",
+        rarity: "Rare",
+        type: "Addon"
+    },
+    {
+        name: "Very Rare Flashlight Add-on",
+        rarity: "Epic",
+        type: "Addon"
+    },
+    {
+        name: "Ultra Rare Flashlight Add-on",
+        rarity: "Legendary",
+        type: "Addon"
+    },
+
+    {
+        name: "Common Toolbox Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Uncommon Toolbox Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Rare Toolbox Add-on",
+        rarity: "Rare",
+        type: "Addon"
+    },
+    {
+        name: "Very Rare Toolbox Add-on",
+        rarity: "Epic",
+        type: "Addon"
+    },
+    {
+        name: "Ultra Rare Toolbox Add-on",
+        rarity: "Legendary",
+        type: "Addon"
+    },
+
+    {
+        name: "Common Fog Vial Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Uncommon Fog Vial Add-on",
+        rarity: "Common",
+        type: "Addon"
+    },
+    {
+        name: "Rare Fog Vial Add-on",
+        rarity: "Rare",
+        type: "Addon"
+    },
+    {
+        name: "Very Rare Fog Vial Add-on",
+        rarity: "Epic",
+        type: "Addon"
+    },
+    {
+        name: "Ultra Rare Fog Vial Add-on",
+        rarity: "Legendary",
+        type: "Addon"
+    }
+);
---
+++ data/perks_03.js
@@ -0,0 +1,71 @@
+gameData.items.push(
+
+    // Med-Kits
+    {
+        name: "Common Med-Kit",
+        rarity: "Common",
+        type: "Item"
+    },
+    {
+        name: "Uncommon Med-Kit",
+        rarity: "Common",
+        type: "Item"
+    },
+    {
+        name: "Rare Med-Kit",
+        rarity: "Rare",
+        type: "Item"
+    },
+    {
+        name: "Very Rare Med-Kit",
+        rarity: "Epic",
+        type: "Item"
+    },
+
+    // Flashlights
+    {
+        name: "Uncommon Flashlight",
+        rarity: "Common",
+        type: "Item"
+    },
+    {
+        name: "Rare Flashlight",
+        rarity: "Rare",
+        type: "Item"
+    },
+    {
+        name: "Very Rare Flashlight",
+        rarity: "Epic",
+        type: "Item"
+    },
+
+    // Toolboxes
+    {
+        name: "Common Toolbox",
+        rarity: "Common",
+        type: "Item"
+    },
+    {
+        name: "Uncommon Toolbox",
+        rarity: "Common",
+        type: "Item"
+    },
+    {
+        name: "Rare Toolbox",
+        rarity: "Rare",
+        type: "Item"
+    },
+    {
+        name: "Very Rare Toolbox",
+        rarity: "Epic",
+        type: "Item"
+    },
+
+    // Fog Vials
+    {
+        name: "Rare Fog Vial",
+        rarity: "Rare",
+        type: "Item"
+    }
+
+);
