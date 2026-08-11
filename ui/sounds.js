e4c7abee094302b6093036ef03c7463f3f13c90b
TTVGrimlock1975 <slenderp1975@gmail.com>
2026-08-11 18:01:09 -0400

    volume fix

--- ui/sounds.js
+++ ui/sounds.js
@@ -7,8 +7,8 @@ PL.sounds = (function () {
     const cardFlip = new Audio("sounds/card-flip.wav");
 
     click.volume = 0.35;
-    packRip.volume = 0.70;
-    cardFlip.volume = 0.50;
+    packRip.volume = 0.30;
+    cardFlip.volume = 0.30;
 
     function play(sound) {
 
